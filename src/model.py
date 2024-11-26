1d9c8526edf76e0d1d6652327630328b190b5c6e
matevaradi <varadimatee@gmail.com>
2024-11-26 20:47:41 +0100

    Finish refactoring

--- src/model.py
+++ src/model.py
@@ -1,27 +1,23 @@
+import numpy as np
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegressionCV
-from sklearn.metrics import (
-    classification_report,
-    confusion_matrix,
-    recall_score,
-    roc_auc_score,
-)
-from sklearn.model_selection import train_test_split
+from sklearn.metrics import recall_score, roc_auc_score
 
 
 class OscarPredictor:
     def __init__(
         self,
-        new_season,
         model_category,
         config,
+        new_season=None,
         model_type="logit",
-        split_type="use_all",
+        verbose=False,
     ):
 
         self.new_season = new_season
         self.cfg = config
+        self.verbose = verbose
 
         if model_category in [
             "Picture",
@@ -33,9 +29,9 @@ class OscarPredictor:
             self.oscar_categories = self.cfg["category_map"][self.model_category]
         else:
             raise ValueError(f"Category: {model_category} not recognized")
-        print(f"Setting up Predictor for {model_category}")
+        if self.verbose:
+            print(f"Setting up Predictor for {model_category}")
 
-        self.split_type = split_type
         self.model_type = model_type
 
         # Load which predictors to use
@@ -62,26 +58,7 @@ class OscarPredictor:
 
         return df_new
 
-    def prepare_train_data(self, df_train):
-        X = df_train[self.predictors]
-        y = df_train["Winner"]
-
-        if self.split_type == "use_all":
-            X_train = X
-            y_train = y
-            df_train = df_train
-        else:
-            raise ValueError(f"split_type : {self.split_type} not implemented")
-            # TODO implement other splits
-
-        return df_train, X_train, y_train
-
-    def prepare_pred_data(self, df_new):
-        X_pred = df_new[self.predictors]
-
-        return X_pred
-
-    def fit_model(self, X, y):
+    def define_model(self):
         if self.model_type.lower() in ["logit", "logistic regression"]:
             model = LogisticRegressionCV(max_iter=5000, solver="newton-cg")
         elif self.model_type.lower() in ["rf", "random forest"]:
@@ -89,34 +66,39 @@ class OscarPredictor:
         else:
             raise ValueError(f"Model type {self.model_type} not supported")
 
-        model.fit(X, y)
-        self.model = model
+        return model
 
     def predict_new_season(self):
         if not hasattr(self, "model"):
             raise ValueError("Model needs to be trained first")
 
         df_new = self.load_new_data()
-        X_pred = self.prepare_pred_data(df_new)
-        df_res = self.get_predictions(X_pred, df_new)
+        X_pred = df_new[self.predictors]
+        df_res = self.get_predictions(self.model, df_new)
 
         self.df_res_new = df_res
 
-    def train_model(self):
+    def train_model_cv(self, model=None):
+        if model is None:
+            model = self.define_model()
 
         df_train = self.load_train_data()
-        df_train, X_train, y_train = self.prepare_train_data(df_train)
-        self.fit_model(X_train, y_train)
-        df_res = self.get_predictions(X_train, df_train)
 
-        # Evaluate results
-        self.eval(df_res)
-        self.df_res_train = df_res
+        # Get CV metrics
+        metrics = self.eval_cv(df_train, model)
 
-    def get_predictions(self, X_pred, df_pred):
+        # Retrain on full data
+        X = df_train[self.predictors]
+        y = df_train["Winner"]
+        model.fit(X, y)
+        self.model = model
+        df_res = self.get_predictions(model, df_train)
+
+    def get_predictions(self, model, df_pred):
 
         df_res = df_pred.copy()
-        probs = self.model.predict_proba(X_pred)[:, 1]
+        X_pred = df_res[self.predictors]
+        probs = model.predict_proba(X_pred)[:, 1]
         df_res["Prob"] = probs
 
         # Classify the film with the highest probability of winning as the winner
@@ -138,5 +120,47 @@ class OscarPredictor:
         return df_res
 
     def eval(self, df_res):
-        print("AUC", roc_auc_score(df_res["Winner"], df_res["Classification"]))
-        print("TPR", recall_score(df_res["Winner"], df_res["Classification"]))
+        auc_score = roc_auc_score(df_res["Winner"], df_res["Classification"])
+        tpr_score = recall_score(df_res["Winner"], df_res["Classification"])
+        print(f"AUC: {auc_score:.2f}, Recall: {tpr_score:.2f}")
+
+    def eval_cv(self, df, model, n_splits=5):
+
+        all_years = pd.Series(sorted(df["Year"].unique()))
+        tscv = TimeSeriesSplit(test_size=5)
+
+        auc_scores = []
+        tpr_scores = []
+        for fold, (train_idx, val_idx) in enumerate(tscv.split(all_years)):
+            train_years = all_years[train_idx].tolist()
+            val_years = all_years[val_idx].tolist()
+            df_train = df.loc[df["Year"].isin(train_years)]
+            df_val = df.loc[df["Year"].isin(val_years)]
+            X_train, X_val = df_train[self.predictors], df_val[self.predictors]
+            y_train, y_val = df_train["Winner"], df_val["Winner"]
+
+            # Train model
+            model.fit(X_train, y_train)
+
+            # Predict on validation data
+            df_res = self.get_predictions(model, df_val)
+            y_val_hat = df_res["Classification"]
+
+            # Evaluate
+            auc_score = roc_auc_score(y_val, y_val_hat)
+            tpr_score = recall_score(y_val, y_val_hat)
+            if self.verbose:
+                print(f"AUC on fold {fold+1}: {auc_score:.2f}")
+                print(f"Recall on fold {fold+1}: {tpr_score:.2f}")
+            auc_scores.append(auc_score)
+            tpr_scores.append(tpr_score)
+
+        # Aggregate metrics
+        avg_auc = np.mean(auc_scores)
+        avg_tpr = np.mean(tpr_scores)
+        metrics = {"AUC": avg_auc, "TPR": avg_tpr}
+        print(
+            f"Cross-Validated model results: AUC: {avg_auc:.2f}, Recall: {avg_tpr:.2f}"
+        )
+
+        return metrics
